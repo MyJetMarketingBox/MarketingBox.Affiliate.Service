74b574ccc05f9ef3c8060d236edc6f863a1b15ad
Lev Kurbanov <lev.k@smplt.net>
2022-04-08 13:27:44 +0500

    fix mapping

--- src/MarketingBox.Affiliate.Service/MapperProfiles/AffiliateMapperProfile.cs
+++ src/MarketingBox.Affiliate.Service/MapperProfiles/AffiliateMapperProfile.cs
@@ -20,10 +20,10 @@ namespace MarketingBox.Affiliate.Service.MapperProfiles
                     s => s.MapFrom(_ => DateTime.UtcNow))
                 .ForMember(d => d.ApiKey,
                     s => s.MapFrom(_ => Guid.NewGuid().ToString("N")))
-                .IncludeMembers(x=>x.GeneralInfo);
-            
+                .IncludeMembers(x => x.GeneralInfo);
+
             CreateMap<AffiliateUpdateRequest, Domain.Models.Affiliates.Affiliate>()
-                .IncludeMembers(x=>x.GeneralInfo);
+                .IncludeMembers(x => x.GeneralInfo);
 
             CreateMap<GeneralInfoRequest, Domain.Models.Affiliates.Affiliate>()
                 .ForMember(x => x.State, x => x.MapFrom(z => z.State ?? State.Active))
@@ -32,6 +32,7 @@ namespace MarketingBox.Affiliate.Service.MapperProfiles
                 .ForMember(x => x.State,
                     x => x.MapFrom(x => State.NotActive));
             CreateMap<Domain.Models.Affiliates.Affiliate, AffiliateMessage>()
+                .ForMember(x => x.AffiliateId, x => x.MapFrom(z => z.Id))
                 .ForMember(x => x.GeneralInfo,
                     x => x.MapFrom(z => z));
             CreateMap<Domain.Models.Affiliates.Affiliate, GeneralInfo>();
